1ac14db72be55c2322ef57c0143b7ac066d1acbb
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-04-23 12:13:25 -0300

    feat:  developed the functionality that takes the parameter within the request

--- src/middlewares/routeHandler.js
+++ src/middlewares/routeHandler.js
@@ -1,11 +1,19 @@
+import { DataBase } from "../database/database.js";
 import { routes } from "../routes/index.js";
+import { extractQueryParams } from "../utils/extractQueryParams.js";
+
+const database = new DataBase();
 
 export function routeHandler(request, response) {
   const route = routes.find((route) => {
-    return route.method === request.method && route.path === request.url;
+    return route.method === request.method && route.path.test(request.url);
   });
   if (route) {
-    return route.controller({ request, response });
+    const routeParams = request.url.match(route.path);
+    const { query } = routeParams.groups;
+    request.query = query ? extractQueryParams(query) : {};
+
+    return route.controller({ request, response, database });
   }
   response.writeHead(404).end();
 }
